3135b0528116f53f18532eb44c08e7b505f3c633
Yashwant Dhakad <yashmitm1231@gmail.com>
2026-01-18 11:15:48 +0530

    Restructure the code as per best practise.

--- oms-service/src/main/java/com/monash/erp/oms/entity/CustomerOrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.monash.erp.oms.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
-}
--- wms-service/src/main/java/com/monash/erp/wms/entity/OldInventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monash.erp.wms.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface OldInventoryRepository extends JpaRepository<OldInventoryItem, Long> {
-    Optional<OldInventoryItem> findBySku(String sku);
-}
